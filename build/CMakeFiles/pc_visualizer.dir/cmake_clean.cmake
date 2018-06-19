dd940e1e5c18316b259d54863ea419d29be6cbd4
youliang <tan_you_liang@hotmail.com>
2018-06-19 10:12:59 +0800

    create codes

---
+++ build/CMakeFiles/pc_visualizer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pc_visualizer.dir/pc_visualizer.cpp.o"
+  "pc_visualizer.pdb"
+  "pc_visualizer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pc_visualizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
